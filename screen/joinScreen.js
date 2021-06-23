8b6332e45fabf8989996153f0dd33b7083843668
yymin1022 <Yymin1022@gmail.com>
2021-06-24 02:37:14 +0900

    Update joinScreen.js : Add Base Layout

--- screen/joinScreen.js
+++ screen/joinScreen.js
@@ -3,8 +3,9 @@ import {StyleSheet, Text, View, Button} from 'react-native';
 
 const JoinScreen = ({navigation, route}) => {
   return (
-    <View>
-        <Text>Join Screen</Text>
-    </View>);
+      <View>
+          <Text>Join Screen</Text>
+      </View>
+    );
 };
 export default JoinScreen;
